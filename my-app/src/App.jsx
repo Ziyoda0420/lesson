4ae8b144357e7fd403dabfac2490d959bf368402
Ziyoda0420 <mirzakirovaziyoda@gmail.com>
2025-06-19 15:03:04 +0500

    adsada

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+import Image from './components/Image'
+
+function App() {
+  return (
+    <div>
+    <Main />
+   <Image />
+    </div>
+   
+  )
+}
+
+export default App
